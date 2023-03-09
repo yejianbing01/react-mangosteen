686eb86ac7d96fdff419327e199a3ac977aa35ce
yejb <yejb_01@163.com>
2023-03-09 21:25:29 +0800

    404页面

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,11 @@
+export default function ErrorPage() {
+  return (
+        <div id="error-page">
+            <h1>哎呀!</h1>
+            <p>页面找不到啦</p>
+            <p>
+                <i>404</i>
+            </p>
+        </div>
+  )
+}
