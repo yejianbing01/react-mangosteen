7b9ce8e7ce11ede0661ba6de92403644bf230f61
yejb <yejb_01@163.com>
2023-03-18 15:34:45 +0800

    拆分欢迎页面路由

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
-import ErrorPage from '../components/ErrorPage'
+import ErrorPage from '../pages/ErrorPage'
 import { MainLayout } from '../layouts/MainLayout'
-import { WelcomeLayout } from '../layouts/WelcomeLayout'
-import { Welcome1 } from '../pages/Welcome1'
-import { Welcome2 } from '../pages/Welcome2'
-import { Welcome3 } from '../pages/Welcome3'
-import { Welcome4 } from '../pages/Welcome4'
+import { welcomeRoutes } from './welcomeRoutes'
 
 export const router = createBrowserRouter([
   {
@@ -13,16 +9,7 @@ export const router = createBrowserRouter([
     element: <MainLayout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        path: 'welcome',
-        element: <WelcomeLayout />,
-        children: [
-          { path: '1', element: <Welcome1 /> },
-          { path: '2', element: <Welcome2 /> },
-          { path: '3', element: <Welcome3 /> },
-          { path: '4', element: <Welcome4 /> },
-        ]
-      },
+      welcomeRoutes
     ]
   },
 ])
