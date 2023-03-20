7c2425360e6614ea90d687dfba4d6711fca0a8ad
yejb <yejb_01@163.com>
2023-03-20 23:26:57 +0800

    完成ItemsSummary组件

--- src/pages/ItemsPage/components/ItemsSummary.tsx
+++ src/pages/ItemsPage/components/ItemsSummary.tsx
@@ -2,8 +2,19 @@ import type { FC } from 'react'
 
 export const ItemsSummary: FC = () => {
   return (
-		<div>
-			ItemsSummary
-		</div>
+		<ol bg="#252A43" flex justify-around items-center m-16px rounded-8px py-12px text-center >
+      <li text="#FE7275">
+        <div>收入</div>
+        <div>1000</div>
+      </li>
+      <li text="#53A867">
+        <div>支出</div>
+        <div>1000</div>
+      </li>
+      <li text-white>
+        <div>净收入</div>
+        <div>1000</div>
+      </li>
+		</ol>
   )
 }
