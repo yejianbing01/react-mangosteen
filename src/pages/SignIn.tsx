80d88a1faf0f32c7f142283d18a706683af6589b
yejb <yejb_01@163.com>
2023-03-27 23:13:48 +0800

    增加validate验证表单字段

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,6 +2,7 @@ import type { FC, FormEventHandler } from 'react'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
 import { useTitle } from '../hooks/useTitle'
+import { validate } from '../lib/validate'
 import { useSignInStore } from '../stores/useSignIdStore'
 
 interface Props {
@@ -9,10 +10,18 @@ interface Props {
 }
 export const SignIn: FC<Props> = ({ title }) => {
   useTitle(title)
-  const { data, setData } = useSignInStore()
+  const { data, setData, error, setError } = useSignInStore()
 
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
+
+    const error = validate(data, [
+      { key: 'email', type: 'required', message: '邮箱不能为空' },
+      { key: 'email', type: 'pattern', regex: /^.+@.+$/, message: '邮箱格式不正确' },
+      { key: 'code', type: 'required', message: '验证码不能为空' },
+      { key: 'code', type: 'length', min: 6, max: 6, message: '验证码为6位' }
+    ])
+    setError(error)
   }
 
   return (
@@ -26,13 +35,13 @@ export const SignIn: FC<Props> = ({ title }) => {
       </div>
       <form j-form onSubmit={onSubmit}>
         <div>
-          <span j-form-label>邮箱地址</span>
+          <span j-form-label>邮箱地址{error.email?.[0] && <span text-red >{error.email[0]}</span>}</span>
           <input value={data.email} type="text" placeholder='请输入邮箱，再点击发送验证码'j-form-input
             onChange={e => setData({ email: e.target.value })}
           />
         </div>
         <div>
-          <span j-form-label>验证码</span>
+          <span j-form-label>验证码{error.code?.[0] && <span text-red>{error.code[0]}</span>}</span>
           <div flex gap-x-16px>
             <input value={data.code} type="text" placeholder='六位数字' w-128px j-form-input
               onChange={e => setData({ code: e.target.value })}
--- src/stores/useSignIdStore.tsx
+++ src/stores/useSignIdStore.tsx
@@ -1,12 +1,16 @@
 import { create } from 'zustand'
+import type { FormError } from '../lib/validate'
 
 type Data = {
   email: string
   code: string
 }
+
 interface SignInData {
   data: Data
+  error: FormError<Data>
   setData: (data: Partial<Data>) => void
+  setError: (error: Partial<FormError<Data>>) => void
 }
 
 export const useSignInStore = create<SignInData>(set => ({
@@ -14,10 +18,21 @@ export const useSignInStore = create<SignInData>(set => ({
     email: '',
     code: ''
   },
+  error: {
+    email: [],
+    code: []
+  },
   setData: data => set(state => ({
+    ...state,
     data: {
       ...state.data,
       ...data
     }
   })),
+  setError: error => set(state => ({
+    ...state,
+    error: {
+      ...error
+    }
+  }))
 }))
