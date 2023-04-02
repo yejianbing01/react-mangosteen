ca85b1224c4dfef0893a19424c3bdcc2c327f260
yejb <yejb_01@163.com>
2023-04-02 16:10:13 +0800

    允许输入金额

--- src/pages/ItemsNewPage/DateAndAmount.tsx
+++ src/pages/ItemsNewPage/DateAndAmount.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import { useState } from 'react'
 import { Icon } from '../../components/Icon'
 
 interface Props {
@@ -7,26 +8,52 @@ interface Props {
   onDateClick?: () => void
 }
 export const DateAndAmount: FC<Props> = ({ date, className, onDateClick }) => {
+  const [output, setOutput] = useState('0')
+
+  const append = (char: string) => {
+    let _output = output
+    switch (char) {
+      case '.':
+        if (!_output.includes(char)) { _output += char }
+        break
+      case '0':
+        if (output !== char) { _output += char }
+        break
+      default:
+        if (_output === '0') {
+          _output = char
+        } else {
+          _output += char
+        }
+        break
+    }
+    if (_output.includes('.') && (_output.length - _output.indexOf('.') > 3)) { return }
+    if (_output.length > 16) { return }
+    setOutput(_output)
+  }
+
+  const clear = () => setOutput('0')
+
   return (
 		<div className={className} >
 			<div flex p-t-15px p-b-16px px-16px border-t-1px border-t="#ddd" gap-x-8px items-center>
 				<Icon name="calendar" className="w-24px h-24px grow-0 shrink-0" onClick={onDateClick} />
 				<span grow-0 shrink-0 text-12px color="#999" onClick={onDateClick}>{date}</span>
-				<code grow-1 shrink-1 text-right color="#53A867">123456789.01</code>
+				<code grow-1 shrink-1 text-right color="#53A867">{output}</code>
 			</div>
 			<div py-1px grid grid-rows="[repeat(4,56px)]" grid-cols-4 bg="#ddd" gap-1px children-bg-white >
-				<button row-start-1 col-start-1 row-end-2 col-end-2 >1</button>
-				<button row-start-1 col-start-2 row-end-2 col-end-3 >2</button>
-				<button row-start-1 col-start-3 row-end-2 col-end-4 >3</button>
-				<button row-start-2 col-start-1 row-end-3 col-end-2 >4</button>
-				<button row-start-2 col-start-2 row-end-3 col-end-3 >5</button>
-				<button row-start-2 col-start-3 row-end-3 col-end-4 >6</button>
-				<button row-start-3 col-start-1 row-end-4 col-end-2 >7</button>
-				<button row-start-3 col-start-2 row-end-4 col-end-3 >8</button>
-				<button row-start-3 col-start-3 row-end-4 col-end-4 >9</button>
-				<button row-start-4 col-start-1 row-end-5 col-end-3 >0</button>
-				<button row-start-4 col-start-3 row-end-5 col-end-4 >.</button>
-				<button row-start-1 col-start-4 row-end-3 col-end-5 >清空</button>
+				<button row-start-1 col-start-1 row-end-2 col-end-2 onClick={() => append('1')} >1</button>
+				<button row-start-1 col-start-2 row-end-2 col-end-3 onClick={() => append('2')} >2</button>
+				<button row-start-1 col-start-3 row-end-2 col-end-4 onClick={() => append('3')} >3</button>
+				<button row-start-2 col-start-1 row-end-3 col-end-2 onClick={() => append('4')} >4</button>
+				<button row-start-2 col-start-2 row-end-3 col-end-3 onClick={() => append('5')} >5</button>
+				<button row-start-2 col-start-3 row-end-3 col-end-4 onClick={() => append('6')} >6</button>
+				<button row-start-3 col-start-1 row-end-4 col-end-2 onClick={() => append('7')} >7</button>
+				<button row-start-3 col-start-2 row-end-4 col-end-3 onClick={() => append('8')} >8</button>
+				<button row-start-3 col-start-3 row-end-4 col-end-4 onClick={() => append('9')} >9</button>
+				<button row-start-4 col-start-1 row-end-5 col-end-3 onClick={() => append('0')} >0</button>
+				<button row-start-4 col-start-3 row-end-5 col-end-4 onClick={() => append('.')} >.</button>
+				<button row-start-1 col-start-4 row-end-3 col-end-5 onClick={clear}>清空</button>
 				<button row-start-3 col-start-4 row-end-5 col-end-5 >提交</button>
 			</div>
 		</div>
