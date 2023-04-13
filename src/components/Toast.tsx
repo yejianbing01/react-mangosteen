46bab42cb5afec07a68d42133b30eb47cb64c388
yejb <yejb_01@163.com>
2023-04-13 21:56:00 +0800

    axios统一错误处理

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,3 +1,4 @@
+import type { FC, ReactNode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Loading } from './Loading'
 
@@ -9,21 +10,30 @@ if (!toastWrapper) {
 }
 const toastRoot = ReactDOM.createRoot(toastWrapper)
 
+type ToastContentProps = {
+  children?: ReactNode
+}
+export const ToastContent: FC<ToastContentProps> = (props) => {
+  return (
+    <div fixed top-0 left-0 h-screen w-screen flex items-center justify-center bg-transparent >
+			<div w="50vw" h="50vw" bg-black bg-opacity-70 rounded-8px flex items-center justify-center text-white >
+        {props.children}
+			</div>
+		</div>
+  )
+}
+
 export const Toast = {
 
-  loading: () => {
-    toastRoot.render(
-			<div fixed top-0 left-0 h-screen w-screen flex items-center justify-center bg-transparent >
-				<div w="50vw" h="50vw" bg-black bg-opacity-70 rounded-8px flex items-center justify-center text-white >
-					<Loading />
-				</div>
-			</div>
-    )
+  loading: () => toastRoot.render(<ToastContent><Loading /></ToastContent>),
+
+  info: (title: string, timeout?: number) => {
+    toastRoot.render(<ToastContent>{title}</ToastContent>)
+    if (timeout) { setTimeout(() => Toast.hide(), timeout) }
   },
 
   hide: () => {
-    if (!toastWrapper) { return }
-    toastRoot.render('')
+    if (toastWrapper) { toastRoot.render('') }
   }
 
 }
--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -1,7 +1,8 @@
+import type { AxiosError } from 'axios'
 import axios from 'axios'
 import { Toast } from '../components/Toast'
 
-axios.defaults.baseURL = isDev ? 'http://121.196.236.94:8080/' : 'https://121.196.236.94:8080/'
+axios.defaults.baseURL = isDev ? 'https://121.196.236.94:8080/' : 'https://121.196.236.94:8080/'
 axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
@@ -12,11 +13,17 @@ axios.interceptors.request.use((config) => {
   }
   Toast.loading()
   return config
+}, (error: AxiosError) => {
+  Toast.info(error.message, 1000)
+  return Promise.reject(error)
 })
 
 axios.interceptors.response.use((value) => {
   Toast.hide()
   return value
+}, (error: AxiosError) => {
+  Toast.info(error.message, 1000)
+  return Promise.reject(error)
 })
 
 export const ajax = {
