00817a49d0a1127c2e935368cf880ca1f46df0e8
yejb <yejb_01@163.com>
2023-04-13 21:14:31 +0800

    消除Toast组件警告

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,28 +1,29 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { Loading } from './Loading'
 
 let toastWrapper = document.getElementById('toastWrapper')
+if (!toastWrapper) {
+  toastWrapper = document.createElement('div')
+  toastWrapper.id = 'toastWrapper'
+  document.body.appendChild(toastWrapper)
+}
+const toastRoot = ReactDOM.createRoot(toastWrapper)
 
 export const Toast = {
 
   loading: () => {
-    if (!toastWrapper) {
-      toastWrapper = document.createElement('div')
-      toastWrapper.id = 'toastWrapper'
-      document.body.appendChild(toastWrapper)
-    }
-    ReactDOM.render(
+    toastRoot.render(
 			<div fixed top-0 left-0 h-screen w-screen flex items-center justify-center bg-transparent >
 				<div w="50vw" h="50vw" bg-black bg-opacity-70 rounded-8px flex items-center justify-center text-white >
 					<Loading />
 				</div>
-			</div>,
-			toastWrapper)
+			</div>
+    )
   },
 
   hide: () => {
     if (!toastWrapper) { return }
-    ReactDOM.unmountComponentAtNode(toastWrapper)
+    toastRoot.unmount()
   }
 
 }
