5bfd6f257475f3a8387b8479e7d4339b01fb9e7e
yejianbin <yejb_01@163.com>
2023-05-10 09:50:30 +0800

    增加vconsole查询日志

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import vhCheck from 'vh-check'
+import VConsole from 'vconsole'
 import { router } from './routes/router'
+
 import './styles/public.scss'
 import 'virtual:uno.css'
 import './styles/main.scss'
 import 'virtual:svgsprites'
 
 vhCheck()
+new VConsole()
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
